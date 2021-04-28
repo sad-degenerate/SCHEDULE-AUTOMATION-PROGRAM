109d79d6c8e4eb67fbb5570a2c8a3769c90519bb
Maxim Babakov <mister.franz02@gmail.com>
2021-04-29 01:28:23 +0700

    Реализованы классы для работы с базой данных.

--- BL/Commands/Delete.cs
+++ BL/Commands/Delete.cs
@@ -22,5 +22,50 @@ namespace BL.Commands
                 context.SaveChanges();
             }
         }
+
+        public static void Equipment(List<Equipment> equipment)
+        {
+            using (var context = new MyDbContext())
+            {
+                context.Equipment.RemoveRange(equipment);
+                context.SaveChanges();
+            }
+        }
+
+        public static void Subject(List<Subject> subjects)
+        {
+            using (var context = new MyDbContext())
+            {
+                context.Subjects.RemoveRange(subjects);
+                context.SaveChanges();
+            }
+        }
+
+        public static void GroupsLoad(List<GroupsLoad> groupsLoads)
+        {
+            using (var context = new MyDbContext())
+            {
+                context.GroupsLoads.RemoveRange(groupsLoads);
+                context.SaveChanges();
+            }
+        }
+
+        public static void TeachersLoad(List<TeachersLoad> teachersLoads)
+        {
+            using (var context = new MyDbContext())
+            {
+                context.TeachersLoads.RemoveRange(teachersLoads);
+                context.SaveChanges();
+            }
+        }
+
+        public static void Classroom(List<Classroom> classrooms)
+        {
+            using (var context = new MyDbContext())
+            {
+                context.Classrooms.RemoveRange(classrooms);
+                context.SaveChanges();
+            }
+        }
     }
 }
